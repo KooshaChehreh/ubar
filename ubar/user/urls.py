45faf257ecf7564b5c7aa37d9d94ddd4f04590a3
kooshachehreh <koosha.bagherzadeh@gmail.com>
2025-04-17 19:38:29 +0330

    urls added

--- ubar/user/urls.py
+++ ubar/user/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from user.views import (
     login_obtain_otp,
     login_verify_password,
+    profile,
+    login_verify_otp
 )
 
 urlpatterns = [
     path("login/obtain-otp", login_obtain_otp, name="login-obtain-otp"),
     path("login/verify-password", login_verify_password, name="login-verify-password"),
+    path("login/profile", profile, name="login-profile"),
+    path("login/verify-otp", login_verify_otp, name="login-verify-otp"),
+
 ]
